b4a7234f482ac57cdadc543a84b23f1c6610d520
AndreaCarollo <andrea.carollo@studenti.unitn.it>
2020-02-26 19:55:48 +0100

    add trained features files

--- peopledect.cpp
+++ peopledect.cpp
@@ -6,13 +6,14 @@ using namespace std;
 int main()
 {
     // String haar_fullbody = "/home/andrea/opencv/sources/opencv/data/haarcascades/haarcascade_fullbody.xml";
-    String haar_fullbody = "/home/andrea/Desktop/haarcascade_pedestrian.xml";
+    String haar_pedestrian = "../trained/haarcascade_pedestrian.xml";
     CascadeClassifier people_cascade; // Class for the cascade classifier
 
     // VideoCapture cap("vtest.avi");
-    VideoCapture sequence("../../Dataset/Our_Video/test1.mp4"); // convert into a realsense video streaming
+    VideoCapture sequence("/home/andrea/Documents/Robotics/Dataset/Our_Video/test1.mp4"); // convert into a realsense video streaming
     Mat frame, frame_gray;
-    people_cascade.load(haar_fullbody);
+    // people_cascade.load(haar_fullbody);
+    people_cascade.load(haar_pedestrian);
 
     for (int i = 0; i < 1000; i++)
     {
@@ -38,6 +39,8 @@ int main()
             rectangle(frame, peoples[j], Scalar(255, 0, 0), 3, 8, 0);
             // ellipse(frame, center, Size(peoples[j].width * 0.5, peoples[j].height * 0.5), 0, 0, 360, Scalar(255, 0, 0), 4, 8, 0);
         }
+
+
 
         imshow("Video", frame);
         if (waitKey(10) == 27)
