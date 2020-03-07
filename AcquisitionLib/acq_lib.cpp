0c45df0214a38ea1494299d7c63e9ab581674fd5
AndreaCarollo <andrea.carollo@studenti.unitn.it>
2020-03-07 19:32:02 +0100

    add lib acquisition

---
+++ AcquisitionLib/acq_lib.cpp
@@ -0,0 +1,123 @@
+#include "./acq_lib.h"
+
+
+#define IMAGE_WIDTH 640
+#define IMAGE_HEIGTH 480
+#define FRAME_RATE 30
+
+cv::Size IMAGE_SIZE = cv::Size(IMAGE_WIDTH, IMAGE_HEIGTH);
+
+
+// --------------- FUNCTIONS --------------- //
+
+/* Set the camera settings */
+void camSettings(rs2::config *cfg){
+
+    //Add desired streams to configuration
+    cfg->enable_stream(RS2_STREAM_COLOR,    IMAGE_WIDTH, IMAGE_HEIGTH, RS2_FORMAT_BGR8, FRAME_RATE);
+    cfg->enable_stream(RS2_STREAM_INFRARED, IMAGE_WIDTH, IMAGE_HEIGTH, RS2_FORMAT_Y8,   FRAME_RATE);
+    cfg->enable_stream(RS2_STREAM_DEPTH,    IMAGE_WIDTH, IMAGE_HEIGTH, RS2_FORMAT_Z16,  FRAME_RATE);
+
+}
+
+
+/* Get the color frame from the camera and transform it in a OpenCV Mat */
+void RGB_acq(cv::Mat *color_frame, rs2::frameset frames){
+
+    // Acquisition of the color frame
+    rs2::video_frame color = frames.get_infrared_frame();
+    
+    // Convert the rs2 frame in a OpenCV Mat
+    cv::Mat color_frame(IMAGE_SIZE, CV_8UC3, (void *) color.get_data(), cv::Mat::AUTO_STEP);
+}
+
+
+/* Get the infrared frame from the camera and transform it in a OpenCV Mat */
+void IR_acq(cv::Mat *infrared_frame, rs2::frameset frames){
+    
+    // Acquisition of the infrared frame
+    rs2::video_frame infrared = frames.get_infrared_frame();
+    
+    // Convert the rs2 frame in a OpenCV Mat
+    cv::Mat infrared_frame(IMAGE_SIZE, CV_8UC1, (void *) infrared.get_data(), cv::Mat::AUTO_STEP);
+
+}
+
+
+/* Get the depth frame from the camera and transform it in a OpenCV Mat */
+void DEPTH_acq(cv::Mat *depth_frame, rs2::frameset frames){
+
+    // Acquisition of the depth frame
+    rs2::video_frame depth = frames.get_depth_frame();
+
+    // Convert the rs2 frame in a OpenCV Mat
+    cv::Mat depth_frame(IMAGE_SIZE, CV_8UC3, (void *) depth.get_data(), cv::Mat::AUTO_STEP);
+
+}
+
+
+/* Transform an object point in a point cloud */
+PntCld points_to_pcl(const rs2::points& points){
+
+    // Create a point cloud object
+    PntCld cloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+    // Set all the paramethers of the point clouds
+    auto sp = points.get_profile().as<rs2::video_stream_profile>();
+    cloud->width = sp.width();
+    cloud->height = sp.height();
+    cloud->is_dense = false;
+    cloud->points.resize(points.size());
+    auto ptr = points.get_vertices();
+    for (auto& p : cloud->points)
+    {
+        p.x = - ptr->x;
+        p.y = - ptr->y;
+        p.z = ptr->z;
+        ptr++;
+    }
+
+    return cloud;
+}
+
+
+/* Transform a depth image to a point cloud */
+PntCld PC_acq( rs2::frameset frames){
+
+    // Acquisition of the depth frame
+    rs2::video_frame depth    = frames.get_depth_frame();
+
+    // Initialization of some params
+    rs2::pointcloud pc;
+    rs2::points points;
+
+    // Realsense point cloud generation (points object)
+    pc.map_to(depth);
+    points = pc.calculate(depth);
+
+    // Transform the points object of rs2 in a point cloud of pcl
+    PntCld point_cloud = points_to_pcl(points);
+}
+
+
+/* Point cloud visualizer */
+void PCViewer(PntCld cloud, PntCldV viewer){
+
+    /* USE METHOD : 
+        viewer-> initCameraParameters();                // initialize the camera parameters
+        PCViewer(point_cloud, viewer);                  // Function call
+        ...
+        waitKey(1);
+        ...
+        viewer -> removePointCloud("sample cloud");     // Remove the cloud from the window
+    */
+
+    // Set all the parameters of the point cloud viewer and add to it the point cloud that we want to reppresent
+    viewer->setBackgroundColor(0, 0, 0);
+    viewer->addPointCloud<pcl::PointXYZ>(cloud, "sample cloud");
+    viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "sample cloud");
+    viewer->addCoordinateSystem(0.001);
+    // Show the viewer
+    viewer -> spinOnce();
+
+}
---
+++ AcquisitionLib/acq_lib.h
@@ -0,0 +1,70 @@
+
+#ifndef ACQUISITION_LIB_H_
+#define ACQUISITION_LIB_H_
+
+
+// Common used library
+#include <iostream>
+#include <thread>
+
+// OpenCV Library
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+
+// Realsense Library
+#include <librealsense2/rs.hpp>
+#include <librealsense2/rsutil.h>
+#include <librealsense2/hpp/rs_frame.hpp>
+
+// PCL
+#include <pcl/common/common_headers.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/console/parse.h>
+
+
+
+// Definition of a new type variable for the point cloud
+typedef pcl::PointCloud<pcl::PointXYZ>::Ptr PntCld;
+
+// Definition of a new type variable for the point cloud viewer
+typedef pcl::visualization::PCLVisualizer::Ptr PntCldV;
+
+
+
+// --------------- FUNCTIONS --------------- //
+
+/* Set the camera settings */
+void camSettings(rs2::config *cfg);
+
+
+/* Get the color frame from the camera and transform it in a OpenCV Mat */
+void RGB_acq(cv::Mat *color, rs2::frameset frames);
+
+
+/* Get the infrared frame from the camera and transform it in a OpenCV Mat */
+void IR_acq(cv::Mat *infrared, rs2::frameset frames);
+
+
+/* Get the depth frame from the camera and transform it in a OpenCV Mat */
+void DEPTH_acq(cv::Mat *depth, rs2::frameset frames);
+
+
+/* Transform an object point in a point cloud */
+PntCld points_to_pcl(const rs2::points& points);
+
+
+/* Get the depth image from the camera and transform it in a point cloud */
+PntCld PC_acq( rs2::frameset frames);
+
+
+/* Point cloud visualizer */
+void PCViewer(PntCld cloud, PntCldV viewer);
+
+
+#endif
+
+
+
+
