0884de36aeeefaa30e9900a789dadca3e71955b5
fergian94 <gianni.ferrara94@gmail.com>
2020-04-07 21:58:47 +0200

    sample configuration for our project testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include "./lib/followme.h"
-#include "./lib/visualizers.h"
-#include "./lib/segmentation.h"
+#include "./lib/configurator.h"
 
 
 using namespace std;
@@ -12,6 +11,35 @@ using namespace std;
 // --------------------------------------------
 int main (int argc, char** argv)
 {
-    // FINAL MAIN
-    return (0);
+    // Create object of the class ConfigReader
+    ConfigReader* p = ConfigReader::getInstance();
+
+    // parse the configuration file
+    p->parseFile();
+
+    // Dump map on the console after parsing it (for debug)
+    // p->dumpFileValues();
+
+    // Print divider on the console to understand the output properly
+    cout << endl << "=================================================" << endl << endl;
+
+    // Define variables to store the value
+    string name("");
+    string skill("");
+    string website("");
+    float age = 0;
+
+    // Update the variable by the value present in the configuration file.
+    p->getValue("Name", name);
+    p->getValue("Skill", skill);
+    p->getValue("Website", website);
+    p->getValue("Age", age);
+
+    // Variables has been updated. Now print it on the console.
+    cout << "Name    = " << name << endl;
+    cout << "Skill   = " << skill << endl;
+    cout << "Website = " << website << endl;
+    cout << "Age     = " << age << endl;
+
+    return 0;
 }
